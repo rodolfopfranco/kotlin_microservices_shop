7d3c97074359d9259deead1cdb81adbb951e65a4
rodolfopfranco <rodolfofrancolemos@hotmail.com>
2022-06-17 09:21:39 -0300

    Security Usuario Repository, Service, Controller changes

--- apiusuarios/src/main/kotlin/com/pan/apiusuarios/controller/UsuarioController.kt
+++ apiusuarios/src/main/kotlin/com/pan/apiusuarios/controller/UsuarioController.kt
@@ -1,15 +1,26 @@
 package com.pan.apiusuarios.controller
 
 import com.pan.apiusuarios.dto.request.UsuarioRequest
+import com.pan.apiusuarios.dto.security.AutenticacaoDTO
+import com.pan.apiusuarios.service.AuthenticationService
 import com.pan.apiusuarios.service.UsuarioService
 import io.swagger.annotations.ApiOperation
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1/users")
 class UsuarioController(
-    private val usuarioService: UsuarioService
+    private val usuarioService: UsuarioService,
+    private val authenticationService: AuthenticationService
 ) {
 
     @ApiOperation(value = "Retrieve every user on the database")
@@ -39,5 +50,18 @@ class UsuarioController(
     fun removerUsuario(@PathVariable id: Int){
         usuarioService.removerUsuario(id)
     }
+
+    @PostMapping("/login")
+    @ApiOperation(value = "Authenticates User")
+    fun autenticar(@RequestBody authForm: AutenticacaoDTO){
+        ResponseEntity.ok(authenticationService.autenticar(authForm))
+    }
+
+    @PatchMapping("/{id}/activate")
+    fun ativar(@PathVariable id: Int) = ResponseEntity.ok(usuarioService.ativar(id))
+
+    @PatchMapping("/{id}/deactivate")
+    fun desativar(@PathVariable id: Int) = ResponseEntity.ok(usuarioService.desativar(id))
+
 
 }
--- apiusuarios/src/main/kotlin/com/pan/apiusuarios/service/UsuarioService.kt
+++ apiusuarios/src/main/kotlin/com/pan/apiusuarios/service/UsuarioService.kt
@@ -4,6 +4,8 @@ import com.pan.apiusuarios.dto.mapper.UsuarioMapper
 import com.pan.apiusuarios.dto.request.UsuarioRequest
 import com.pan.apiusuarios.entity.Usuario
 import com.pan.apiusuarios.repository.UsuarioRepository
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Service
 import javax.persistence.EntityNotFoundException
 
@@ -11,7 +13,7 @@ import javax.persistence.EntityNotFoundException
 class UsuarioService(
     private val usuarioRepository: UsuarioRepository,
     private val usuarioMapper: UsuarioMapper
-) {
+): UserDetailsService {
 
     fun listarUsuarios(): List<Usuario> = usuarioRepository.findAll()
 
@@ -32,5 +34,33 @@ class UsuarioService(
     }
 
     fun removerUsuario(id: Int) = usuarioRepository.deleteById(id)
+
+    fun buscarUsuarioPorEmail(email: String) = usuarioRepository.findByEmail(email)
+
+    override fun loadUserByUsername(username: String): UserDetails {
+            return buscarUsuarioPorEmail(username).orElseThrow{
+                EntityNotFoundException("Usuário não encontrado com o e-mail informado")
+            }
+    }
+
+    fun buscarUsuarioPorId(idUsuario: Long): Usuario {
+        return usuarioRepository.findById(idUsuario.toInt()).orElseThrow{
+            EntityNotFoundException("Usuário não encontrado com o ID informado")
+        }
+    }
+
+    fun ativar(id: Int): Usuario {
+        var usuarioEncontrado = buscarUsuarioPorId(id.toLong())
+        if(usuarioEncontrado.isActive) throw IllegalArgumentException("Usuário já está ativo")
+        usuarioEncontrado.isActive = true
+        return usuarioRepository.save(usuarioEncontrado)
+    }
+
+    fun desativar(id: Int): Usuario {
+        var usuarioEncontrado = buscarUsuarioPorId(id.toLong())
+        if(!usuarioEncontrado.isActive) throw IllegalArgumentException("Usuário já está inativo")
+        usuarioEncontrado.isActive = false
+        return usuarioRepository.save(usuarioEncontrado)
+    }
 
 }
