0982b7deb29292077e8c8936ea74474494bebeb8
rodolfopfranco <rodolfofrancolemos@hotmail.com>
2022-06-21 12:11:48 -0300

    feat(apiProdutos): Exception Handling

---
+++ apiprodutos/src/main/kotlin/com/pan/apiprodutos/exception/ExceptionHandler.kt
@@ -0,0 +1,14 @@
+package com.pan.apiprodutos.exception
+
+import org.springframework.http.ResponseEntity
+
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+
+@ControllerAdvice
+class ExceptionHandlerAdvice {
+    @ExceptionHandler(ResourceException::class)
+    fun handleException(e: ResourceException): ResponseEntity<*> {
+        return ResponseEntity.status(e.httpStatus).body(e.message)
+    }
+}
