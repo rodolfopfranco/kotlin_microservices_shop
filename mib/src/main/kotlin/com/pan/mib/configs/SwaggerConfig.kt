b5c97d937c02f502de3e05a774bc18c55348d73e
rodolfopfranco <rodolfofrancolemos@hotmail.com>
2022-07-13 16:15:25 -0300

    fix(apiUsuarios): Port, HTTP and Swagger settings

---
+++ mib/src/main/kotlin/com/pan/mib/configs/SwaggerConfig.kt
@@ -0,0 +1,38 @@
+package com.pan.mib.configs
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.bind.annotation.RestController
+import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.service.Contact
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+
+@Configuration
+@EnableSwagger2
+class SwaggerConfig {
+    @Bean
+    fun api(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+            .apiInfo(apiInfo())
+            .select()
+            .apis(RequestHandlerSelectors.withClassAnnotation(RestController::class.java))
+            .paths(PathSelectors.any())
+            .build()
+    }
+
+    private fun apiInfo(): ApiInfo {
+        return ApiInfoBuilder()
+            .title("MIB - Management Information Base")
+            .contact(Contact("Franco", "github.com", "email@email.com"))
+            .version("1.0")
+            .description("API for managing all Kotlin E-Shop microservice's endpoints")
+            .license("Creative Commons")
+            .build()
+    }
+}
