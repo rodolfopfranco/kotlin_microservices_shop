8320944fab565a0de2135ba68992585f0047dffd
rodolfopfranco <rodolfofrancolemos@hotmail.com>
2022-07-13 16:15:59 -0300

    feat(mib): Validation, Swagger and Feign dependencies added

--- mib/build.gradle.kts
+++ mib/build.gradle.kts
@@ -21,6 +21,14 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	// Feign API consumer:
+	implementation("org.springframework.cloud:spring-cloud-starter-openfeign:3.1.3")
+	implementation("io.github.openfeign:feign-okhttp:11.8")
+	// Validation
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	// Documentation Swagger:
+	implementation("io.springfox:springfox-swagger-ui:2.9.2")
+	implementation("io.springfox:springfox-swagger2:2.9.2")
 }
 
 tasks.withType<KotlinCompile> {
